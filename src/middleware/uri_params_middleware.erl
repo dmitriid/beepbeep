982fcdbdce28abf8e2c807efdc9eb34565a73662
Will Larson <lethain@gmail.com>
2009-09-07 21:53:32 +0000

    Added middleware for populating beepbeep.data with query string parameters

---
+++ src/middleware/uri_params_middleware.erl
@@ -0,0 +1,42 @@
+-module(uri_params_middleware).
+%% @doc A middlware for extracting URI parameters and populating beepbeep.data.
+%%      Middleware must preceed beepbeep, but otherwise has no position requirements.
+%%
+%%      Acquired get_param/2 and get_param/3 from beepbeep_args, as beepbeep.data was
+%%      no longer being populated and thus the functions are unusable if this middleware
+%%      is not in effect.
+%%
+%% @author Will Larson <lethain@gmail.com> [lethain.com]
+
+-export([run/2, get_param/2, get_param/3]).
+
+get_param(Key, Env) ->
+    error_logger:info_report({get_param, Key}),
+    get_param(Key, Env, undefined).
+
+get_param(Key, Env, Default) ->
+    error_logger:info_report({get_param, Key, Default}),
+    Params = ewgi_api:find_data("beepbeep.data", Env),
+    case is_list(Params) of
+	true ->
+	    proplists:get_value(Key, Params, Default);
+	false ->
+	    Default
+    end.
+
+%run(Ctx, App) ->
+%    App(Ctx).
+
+run(Ctx, App) ->
+    QueryString = ewgi_api:query_string(Ctx),
+    error_logger:info_report({qs, QueryString}),
+    Data = case is_list(QueryString) of
+	       true ->
+		   mochiweb_util:parse_qs(QueryString);
+	       false ->
+		   []
+    end,
+    error_logger:info_report({storing, Data}),
+    Ctx1 = ewgi_api:store_data("beepbeep.data", Data, Ctx),
+    error_logger:info_report({stored, "beepbeep.data", Data}),
+    App(Ctx1).
